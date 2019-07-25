40d20f2e442005249cf6eb104f755b7fd58cf295
pineur <navatm@gmail.com>
2019-07-25 13:35:35 +0300

    Use admin cert instead of root cert

--- restore-snapshots.py
+++ restore-snapshots.py
@@ -4,23 +4,23 @@ import sys
 import click
 import logging
 
-from infra.consts import DEFAULT_ROOT_CA_PATH, DEFAULT_ADMIN_KEY_PATH
+from infra.consts import DEFAULT_ADMIN_CERT_PATH, DEFAULT_ADMIN_KEY_PATH
 from infra.log import init_logger
 
 
 @click.command()
-@click.option('-c', '--root-ca-cert', default=DEFAULT_ROOT_CA_PATH, help='Root CA certification path (e.g. {DEFAULT_ROOT_CA_PATH})')
+@click.option('-c', '--admin-cert', default=DEFAULT_ADMIN_CERT_PATH, help='Admin certification path (e.g. {DEFAULT_ADMIN_CERT_PATH})')
 @click.option('-k', '--admin-key', default=DEFAULT_ADMIN_KEY_PATH, help='Admin key path (e.g. {DEFAULT_ADMIN_KEY_PATH})')
 @click.option('-d', '--first', default=None, help='First snapshot name')
 @click.option('-l', '--last', default=None, help='Last snapshot name')
-def main(root_ca_cert, admin_key, first, last):
+def main(admin_cert, admin_key, first, last):
     f"""
     Restore ES snapshots using ROOT_CA_CERT and ADMIN_KEY
     See https://opendistro.github.io/for-elasticsearch-docs/docs/elasticsearch/snapshot-restore/#security-plugin-considerations
     """
     from infra.snapshot import SnapshotClient
 
-    client = SnapshotClient(root_ca_cert, admin_key)
+    client = SnapshotClient(admin_cert, admin_key)
     client.restore_multiple(first, last)
 
 
