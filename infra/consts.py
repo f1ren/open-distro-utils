75e5525f5764d7aed8b2b8fc5b3981ea7b9a3f39
pineur <navatm@gmail.com>
2019-07-25 11:08:44 +0300

    Added defaults for DEFAULT_ROOT_CA_PATH and DEFAULT_ADMIN_KEY_PATH

--- infra/consts.py
+++ infra/consts.py
@@ -4,5 +4,5 @@ DEFAULT_ES_URL = 'https://localhost:9200'
 REPOSITORY_NAME = os.environ.get('ES_SNAPSHOT_REPOSITORY')
 USER = os.environ.get('ES_SNAPSHOT_USER')
 PASS = os.environ.get('ES_SNAPSHOT_PASS')
-DEFAULT_ROOT_CA_PATH = os.environ.get('ES_ROOT_CA_CERT')
-DEFAULT_ADMIN_KEY_PATH = os.environ.get('ES_ADMIN_KEY')
+DEFAULT_ROOT_CA_PATH = os.environ.get('ES_ROOT_CA_CERT', 'root-ca.pem')
+DEFAULT_ADMIN_KEY_PATH = os.environ.get('ES_ADMIN_KEY', 'admin-secret.pem')
