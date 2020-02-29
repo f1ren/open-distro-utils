d1de6be127d6ba0f830d129e888503bf3ab3e149
pineur <navatm@gmail.com>
2020-02-29 17:32:35 +0200

    Moved import, added crontab

--- drop-old-indices.py
+++ drop-old-indices.py
@@ -1,15 +1,17 @@
 import logging
 
-from infra._index import filter_older_than
 from infra.log import init_logger
 
 INDEX_PATTERN = 'transcript-*'
 MONTHS_AGO = 3
 
+# 0 0 2 * * . $HOME/.profile; $(which python3) /home/ubuntu/open-distro-utils/drop-old-indices.py 2>&1
+
 if __name__ == '__main__':
     init_logger()
     try:
         from infra._snapshot import ESClient
+        from infra._index import filter_older_than
         client = ESClient()
         all_indices = client.list_indices(INDEX_PATTERN)
         old_indices = filter_older_than(all_indices, MONTHS_AGO)
